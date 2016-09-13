686ece22bb38c64af31b176acb17757d1271510f
Venryx <venryx@gmail.com>
2016-09-12 22:06:15 -0700

    * Module-file start-lines-in-bundle are now generated and added to a JSON object that's appended to the bundle/chunk files.

--- index.js
+++ index.js
@@ -1,31 +1,94 @@
+var path = require("path");
 var fs = require("fs");
+var ConcatSource = require("webpack-core/lib/ConcatSource");
+
+var Log = console.log;
 
 class WebpackBasicSourcemap {
 	constructor(options) {
+		//this.options = options;
 	}
 	apply(compiler) {
-		// Setup callback for accessing a compilation:
-		compiler.plugin("after-compile", function(compilation) {
-			this.AddBasicSourceMap(compilation.compiler.outputPath + "/Libraries.js");
-			this.AddBasicSourceMap(compilation.compiler.outputPath + "/Main.js");
+		var outputPath =
+			(compiler.options.devServer && compiler.options.devServer.outputPath)
+			|| (compiler.options.output && compiler.options.output.path)
+			|| compiler.options.outputPath;
+		//Log("Output path) " + outputPath);
+		
+		compiler.plugin("compilation", (compilation, params)=> {
+			// add filename banner before each module's text
+			compilation.plugin("optimize-chunk-assets", (chunks, callback)=> {
+				chunks.forEach(chunk=> {
+					chunk.files.forEach(file=> {
+						compilation.assets[file] = new ConcatSource("\/** Filename: " + file.name + " **\/ ", compilation.assets[file]);
+					});
+				});
+				callback();
+			});
+			
+			/*compiler.plugin("after-compile", compilation=> {
+				for (let bundleFileName of this.options.bundleFileNames)
+					this.AddBasicSourceMap(compilation, path.resolve(outputPath, bundleFileName));
+			});*/
+			// add JSON object to end of bundle, that runs and tells the modules what line they each started at
+			compilation.plugin("after-optimize-chunk-assets", chunks=> {
+				/*for (let bundleFileName of this.options.bundleFileNames) {
+					var chunk = chunks.find(a=>a.name == path.basename(bundleFileName, ".js"));
+					this.AddBasicSourceMap(chunk.modules, path.resolve(outputPath, bundleFileName + ".js"));
+				}*/
+				for (let chunk of chunks)
+					this.AddBasicSourceMap(chunk.modules, path.resolve(outputPath, chunk.name + ".js"));
+			});
 		});
 	}
-	AddBasicSourceMap(filePath, storeAsVarName) {
-		var oldText = fs.readFileSync(filePath);
+	AddBasicSourceMap(modules, filePath) {
+		var oldText = fs.readFileSync(filePath, {encoding: "utf8"});
 		var lines = oldText.split("\n");
+		
 		var moduleStartLines = {};
+		var moduleFileCount = 0;
+		var hasOldBasicSourceMap = false;
 		for (let [lineIndex, line] of lines.entries()) {
-			var moduleIndexMatch = line.match(/^\/\* ([0-9]+) \*\/$/);
-			if (moduleIndexMatch) {
-				var moduleIndex = moduleIndexMatch[1];
-				moduleStartLines[moduleIndex] = lineIndex;
+			var moduleIDMatch = line.match(/^\/\* ([0-9]+) \*\/$/);
+			if (moduleIDMatch) {
+				var moduleID = moduleIDMatch[1];
+				var module = modules.find(a=>a.id == moduleID);
+				var moduleFilePath = module.request ? module.request.substr(module.request.lastIndexOf(":") - 1) : "[no path (entry file)]";
+				//var moduleFileName = /([A-Za-z_]+)\.[A-Za-z]$/.exec(moduleFilePath)[1];
+				// offset by 4, since there's some boilerplate-code lines (3 for module-declaration comment, 1 for blank line just after)
+				moduleStartLines[moduleFilePath] = lineIndex + 4;
+				moduleFileCount++;
 			}
+			
+			if (line.startsWith("window.ModuleFileStartLines_"))
+				hasOldBasicSourceMap = true;
 		}
 		
-		debugger;
+		Log("Adding basic source-map to) " + filePath);
+		//debugger;
 		
-		var appendText = "\n\nwindow." + storeAsVarName + " = " + JSON.stringify(moduleStartLines);
-		fs.appendFileSync(filePath, appendText, {encoding: "utf8"});
+		var fileNameWithoutExtension = path.basename(filePath, ".js");
+		var appendText = "\n\nwindow.ModuleFileStartLines_" + fileNameWithoutExtension + " = " + JSON.stringify(moduleStartLines);
+		
+		if (!hasOldBasicSourceMap) {
+			/*//fs.appendFileSync(filePath, appendText, {encoding: "utf8"});
+			// delay a bit (i.e. run after this stack completes), since otherwise our append doesn't apply (there must be a writeFile call after this area of code runs)
+			setTimeout(()=>fs.appendFileSync(filePath, appendText, {encoding: "utf8"}), 0);*/
+			
+			var newText = oldText + appendText;
+			fs.writeFileSync(filePath, newText, {encoding: "utf8"});
+			// try again a bit later, since sometimes (usually/always) doesn't work the first time
+			//setTimeout(()=>fs.writeFileSync(filePath, newText, {encoding: "utf8"}), 1000);
+			// delay a bit (i.e. run after this stack completes), since otherwise our writeFile doesn't apply (there must be a writeFile call after this area of code runs)
+			setTimeout(()=>fs.writeFileSync(filePath, newText, {encoding: "utf8"}), 0);
+		}
+		else {
+			var oldText_withoutBasicSourceMap = lines.slice(0, lines.length - 2).join("\n");
+			var newText = oldText_withoutBasicSourceMap + appendText;
+			fs.writeFileSync(filePath, newText, {encoding: "utf8"});
+		}
+		
+		return moduleFileCount;
 	}
 }
 
